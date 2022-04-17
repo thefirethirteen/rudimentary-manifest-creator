6dcb97562d7771b12e73f04bd99de054758c284a
thefirethirteen <thefirethirteen@protonmail.com>
2022-04-18 00:02:12 +0300

    Get the fabric.mod.json extractor

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+MIT License
+
+Copyright (c) 2022 thefirethirteen
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# setup.py
+# Downloads required scripts
+
+import requests
+
+fabricExtractorUrl = "https://raw.githubusercontent.com/thefirethirteen/fabric.mod.json-extractor/main/main.py"
+
+fabricExtractorWeb = requests.get(fabricExtractorUrl)
+
+with open("fabricExtractor.py", 'wb') as fe:
+    fe.write(fabricExtractorWeb.content)
